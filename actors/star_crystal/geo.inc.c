65fc70ed0432b501bea192e425581fcaeecde6f9
Romcad82 <romcad82@gmail.com>
2024-01-18 17:28:50 -0800

    Fixed Star Crystal Transparency Issues

--- actors/star_crystal/geo.inc.c
+++ actors/star_crystal/geo.inc.c
@@ -5,9 +5,8 @@ const GeoLayout star_crystal_geo[] = {
 	GEO_OPEN_NODE(),
 		GEO_SHADOW(1, 155, 200),
 		GEO_OPEN_NODE(),
-			GEO_DISPLAY_LIST(LAYER_TRANSPARENT_DECAL, star_crystal_pSphere1_node_mesh_layer_6),
+			GEO_DISPLAY_LIST(LAYER_TRANSPARENT, star_crystal_pSphere1_node_mesh_layer_5),
 		GEO_CLOSE_NODE(),
-		GEO_DISPLAY_LIST(LAYER_TRANSPARENT_DECAL, star_crystal_material_revert_render_settings),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };
--- actors/star_crystal/geo_header.h
+++ actors/star_crystal/geo_header.h
@@ -1,8 +1,8 @@
 extern const GeoLayout star_crystal_geo[];
-extern u8 star_crystal_starbox_env1_rgba16[];
-extern Vtx star_crystal_pSphere1_node_mesh_layer_6_vtx_0[108];
-extern Gfx star_crystal_pSphere1_node_mesh_layer_6_tri_0[];
+extern Lights1 star_crystal_f3dlite_material_001_lights;
+extern u8 star_crystal_starbox_env1_rgba32[];
+extern Vtx star_crystal_pSphere1_node_mesh_layer_5_vtx_0[108];
+extern Gfx star_crystal_pSphere1_node_mesh_layer_5_tri_0[];
 extern Gfx mat_star_crystal_f3dlite_material_001[];
 extern Gfx mat_revert_star_crystal_f3dlite_material_001[];
-extern Gfx star_crystal_pSphere1_node_mesh_layer_6[];
-extern Gfx star_crystal_material_revert_render_settings[];
+extern Gfx star_crystal_pSphere1_node_mesh_layer_5[];
